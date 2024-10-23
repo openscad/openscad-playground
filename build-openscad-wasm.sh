3cc0090e9fb24a7a2c82b6ed2458b8610785c83f
Olivier Chafik <ochafik@google.com>
2024-10-23 13:32:50 +0100

    Use Google CDN instead of unpkg for modelviewer

--- build-openscad-wasm.sh
+++ build-openscad-wasm.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 if [ -z "${OPENSCAD_DIR:-}" ]; then
-  OPENSCAD_DIR=~/tmp/openscad-color
+  OPENSCAD_DIR=$HOME/tmp/openscad-color
   if [ ! -d "$OPENSCAD_DIR" ]; then
       rm -fR "$OPENSCAD_DIR"
       git clone --recurse https://github.com/ochafik/openscad.git \
@@ -14,7 +14,7 @@ fi
 docker run --rm -it -v "$OPENSCAD_DIR":/src:rw --platform=linux/amd64 openscad/wasm-base:latest \
   emcmake cmake -B build -DEXPERIMENTAL=ON "$@"
 docker run --rm -it -v "$OPENSCAD_DIR":/src:rw --platform=linux/amd64 openscad/wasm-base:latest \
-  cmake --build build -j10
+  cmake --build build -j2
 
 rm -fR libs/openscad-wasm
 mkdir -p libs/openscad-wasm
