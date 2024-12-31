7ffedccf2ecfefbe80047019555de70498f45c26
Rotzbua <Rotzbua@users.noreply.github.com>
2024-12-31 10:25:45 +0100

    chore(editor): update to latest version

--- src/language/openscad-editor-options.ts
+++ src/language/openscad-editor-options.ts
@@ -16,5 +16,5 @@ export default {
     preview: true,
   },
   codeLens: true,
-  wordBasedSuggestions: false,
+  wordBasedSuggestions: "off",
 } as monaco.editor.IStandaloneEditorConstructionOptions;
