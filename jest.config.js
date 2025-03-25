400b840e55656dbc3274eee7962315b6a777ad93
ochafik <ochafik@google.com>
2025-03-25 23:32:19 +0000

    Disable chrome sandbox on puppeteer test for CI

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,14 @@ module.exports = {
   testMatch: [
     "**/tests/**/*.js",
   ],
+  globals: {
+    'jest-puppeteer': {
+      launch: {
+        args: [
+          // https://chromium.googlesource.com/chromium/src/+/main/docs/security/apparmor-userns-restrictions.md#what-if-i-dont-have-root-access-to-the-machine-and-cant-install-anything
+          '--no-sandbox',
+        ],
+      }
+    }
+  },
 };
